e958897052fa7eaef35ceb9f72c57c5372a339ae
mylest7 <35745113+mylest7@users.noreply.github.com>
2022-05-17 10:45:01 -0400

    Update A_TO_C.sh

--- Setup_Scripts/A_TO_C.sh
+++ Setup_Scripts/A_TO_C.sh
@@ -1,5 +1,7 @@
 #1/bin/bash
 
+# This script is meant to ssh each into a specified range of nodes and switch the access point that it is connected to. 
+# The range and other variables are input by the user. 
 sudo sed -i "4,9d" /etc/wpa_supplicant/wpa_supplicant.conf
 sudo sed -i "4s/.*//" /etc/dhcpcd.conf
 sudo sed -i "5s/.*//" /etc/dhcpcd.conf
